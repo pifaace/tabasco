8f368fae1a4f600875e75d37bfd907483d0272d1
JOASSY Maxime <maxime.joassy@sully-group.fr>
2016-12-12 14:28:44 +0100

    ajout appli android

---
+++ app/src/main/java/com/example/maximejoassy/tabascoappli/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.maximejoassy.tabascoappli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("http://tabasco-team.quentin-reboli.com/");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+}
