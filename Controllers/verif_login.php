b6a4278b5a8f0a4aa03d7ed7460b6fd574c17fb0
JOASSY Maxime <maxime.joassy@sully-group.fr>
2016-12-13 15:08:06 +0100

    test

--- Controllers/verif_login.php
+++ Controllers/verif_login.php
@@ -3,7 +3,7 @@
 	$login = $_POST['login'];
 	$pwd = $_POST['mdp'];
 try{
-	$db = new PDO("mysql:host=https://tabasco-team.labo-g4.fr:2083;dbname=tabasco;charset=utf8","gaubert","841aZYTR2b");
+	$db = new PDO("mysql:host=http://tabasco-team.labo-g4.fr:2083/cpsess8027652924/3rdparty/phpMyAdmin/index.php#PMAURL-1:db_structure.php?db=gaubert_tabasco&table=&server=1&target=&token=52bcd1412e53c98f86920e2e2cfafea2;dbname=tabasco;charset=utf8");
 	}catch(PDOException  $e ){
 	echo "Error: ".$e;
 }
--- Controllers/verif_reponse.php
+++ Controllers/verif_reponse.php
@@ -2,7 +2,7 @@
 
 session_start();
 try{
-$db = new PDO("mysql:host=https://tabasco-team.labo-g4.fr:2083;dbname=tabasco;charset=utf8","gaubert","841aZYTR2b");
+$db = new PDO("mysql:host=http://tabasco-team.labo-g4.fr:2083/cpsess8027652924/3rdparty/phpMyAdmin/index.php#PMAURL-1:db_structure.php?db=gaubert_tabasco&table=&server=1&target=&token=52bcd1412e53c98f86920e2e2cfafea2;dbname=tabasco;charset=utf8");
 }catch(PDOException  $e ){
 echo "Error: ".$e;
 }
