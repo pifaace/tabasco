fa39d5cfa8deffe009fd75c8439262bc5963dc9f
KEVTRO\Kev <kevtro@orange.fr>
2016-12-14 09:56:23 +0100

    fix conflict pour sur

--- accueil.php
+++ accueil.php
@@ -1,12 +1,8 @@
 <?php
 try{
-<<<<<<< HEAD
-  $db = new PDO("mysql:host=localhost;dbname=tabasco;charset=utf8","root","");
-=======
-  //$db = new PDO("mysql:host=localhost;dbname=tabasco;charset=utf8","root","root");
+
   $db = new PDO("mysql:host=localhost;dbname=gaubert_tabasco;charset=utf8","gaubert_test","testtestg4");
 
->>>>>>> 0d321fdeaf777aa279c918928271f28d6ced9907
 }catch(PDOException  $e ){
 echo "Error: ".$e;
 }
